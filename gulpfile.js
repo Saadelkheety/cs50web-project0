cdc0bd8a701c04c4a607eea6ae0532bc3d0b4690
saad elkheety <saadelkheety@gmail.com>
2019-01-15 18:53:59 +0200

    the main frame and crete the gulp file

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const { series, parallel, src, dest, watch } = require('gulp');
+const Sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require("gulp-autoprefixer");
+
+function defaultTask(cb) {
+  watch('./css/*.scss', sass);
+  watch(['./js/*.js', './*.html']).on('change', browserSync.reload);
+  browserSync.init({
+      server: "./"
+    });
+  cb();
+}
+
+function sass(){
+    return src('./css/*.scss')
+    .pipe(Sass().on('error', sass.logError))
+    .pipe(
+          autoprefixer({
+            browsers: ["last 2 versions"]
+          })
+        )
+    .pipe(dest('./css'))
+    .pipe(browserSync.stream());;
+}
+
+exports.default = defaultTask
+// const gulp = require("gulp");
+// const sass = require("gulp-sass");
+// const autoprefixer = require("gulp-autoprefixer");
+// const browserSync = require("browser-sync").create();
+//
+//
+// gulp.task("styles", function(done) {
+//   gulp
+//     .src("css/**/*.scss")
+//     .pipe(sass().on("error", sass.logError))
+//     .pipe(
+//       autoprefixer({
+//         browsers: ["last 2 versions"]
+//       })
+//     )
+//     .pipe(gulp.dest("./css"))
+//     .pipe(browserSync.stream());
+//     done();
+// });
+//
+// gulp.task("default",  gulp.series("styles", function(done) {
+//   gulp.watch("sass/**/*.scss", gulp.series("styles"));
+//
+//   browserSync.init({
+//     server: "./"
+//   });
+//   done();
+// }));
