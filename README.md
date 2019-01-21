0645095dda2a42f18214ac5a6723cd10a5729a7c
Saad Elkheety <SaadElkheety@gmail.com>
2019-01-21 03:27:21 +0200

    end all the required portofolio

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Project 0
-
+- https://elkheety.herokuapp.com
 Web Programming with Python and JavaScript
 
 ## languages:
@@ -13,3 +13,17 @@ Web Programming with Python and JavaScript
 ## tools:
 - gulp
 - NPM
+
+## index.html
+- BOOTSRAP CARD with  image and text.
+
+## education.html
+- carousel to show the certificates.
+- table to show the education.
+
+## skills.html
+- table to show the skills
+- list to show the stack
+
+## portofolio
+- list to show the past projects
--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,85 @@
-const { series, parallel, src, dest, watch } = require('gulp');
+const {
+    series,
+    parallel,
+    src,
+    dest,
+    watch
+} = require('gulp');
 const Sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const autoprefixer = require("gulp-autoprefixer");
+const del = require('del');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const usemin = require('gulp-usemin');
+const rev = require('gulp-rev');
+const cleanCss = require('gulp-clean-css');
+const flatmap = require('gulp-flatmap');
+const htmlmin = require('gulp-htmlmin');
 
 function defaultTask(cb) {
-  watch('./css/*.scss', sass);
-  watch(['./js/*.js', './*.html']).on('change', browserSync.reload);
-  browserSync.init({
-      server: "./"
+    watch('./css/*.scss', sass);
+    watch(['./js/*.js', './*.html']).on('change', browserSync.reload);
+    browserSync.init({
+        server: "./"
     });
-  cb();
+    cb();
 }
 
-function sass(){
+function sass() {
     return src('./css/*.scss')
-    .pipe(Sass().on('error', Sass.logError))
-    .pipe(
-          autoprefixer({
-            browsers: ["last 2 versions"]
-          })
+        .pipe(Sass().on('error', Sass.logError))
+        .pipe(
+            autoprefixer({
+                browsers: ["last 2 versions"]
+            })
         )
-    .pipe(dest('./css'))
-    .pipe(browserSync.stream());;
+        .pipe(dest('./css'))
+        .pipe(browserSync.stream());;
+}
+
+function clean() {
+    return del(['./minifiedVersion/**', '!./minifiedVersion/.git', './minifiedVersion/*.php']);
+}
+
+function copyFiles() {
+    return src('./files/*')
+        .pipe(dest('./minifiedVersion/files'));
+}
+
+function minifyImage() {
+    return src('img/*')
+        .pipe(imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(dest('minifiedVersion/img'));
+}
+
+function minifyHtml() {
+    return src('./*.html')
+        .pipe(usemin({
+            css: [rev],
+            html: [function() {
+                return htmlmin({
+                    collapseWhitespace: true
+                });
+            }],
+            js: [uglify, rev],
+            inlinejs: [uglify],
+            inlinecss: [cleanCss, 'concat']
+        }))
+        .pipe(dest('minifiedVersion/'));
+}
+
+function minifyCss() {
+    return src('css/*.css')
+        .pipe(cleanCss({
+            compatibility: 'ie8'
+        }))
+        .pipe(dest('minifiedVersion/css'));
 }
 
 exports.default = defaultTask
-// const gulp = require("gulp");
-// const sass = require("gulp-sass");
-// const autoprefixer = require("gulp-autoprefixer");
-// const browserSync = require("browser-sync").create();
-//
-//
-// gulp.task("styles", function(done) {
-//   gulp
-//     .src("css/**/*.scss")
-//     .pipe(sass().on("error", sass.logError))
-//     .pipe(
-//       autoprefixer({
-//         browsers: ["last 2 versions"]
-//       })
-//     )
-//     .pipe(gulp.dest("./css"))
-//     .pipe(browserSync.stream());
-//     done();
-// });
-//
-// gulp.task("default",  gulp.series("styles", function(done) {
-//   gulp.watch("sass/**/*.scss", gulp.series("styles"));
-//
-//   browserSync.init({
-//     server: "./"
-//   });
-//   done();
-// }));
+exports.build = series(clean, copyFiles, minifyImage, minifyHtml, minifyCss);
